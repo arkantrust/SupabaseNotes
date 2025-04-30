3bdf65824199a8dcdd6fd8c69043d1e9e4c1cfbd
David Dulce <114516037+arkantrust@users.noreply.github.com>
2025-04-29 23:17:30 -0500

    feat: android project

---
+++ app/src/main/java/app/ddulce/supabasenotes/MainActivity.kt
@@ -0,0 +1,45 @@
+package app.ddulce.supabasenotes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.ddulce.supabasenotes.ui.theme.SupabaseNotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SupabaseNotesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize().padding(4.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NotesList()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NotesList() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotesListPreview() {
+    SupabaseNotesTheme {
+        NotesList()
+    }
+}
