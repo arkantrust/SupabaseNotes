26d23de5c95c6b3f35b7240099f92a8cca59992d
David Dulce <114516037+arkantrust@users.noreply.github.com>
2025-04-30 19:12:12 -0500

    feat: add supabase deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -49,6 +50,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.ktor.client.okhttp)
+    implementation(platform(libs.supabasekt))
+    implementation(libs.postgrestkt)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
